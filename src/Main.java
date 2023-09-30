3678cdd3a17258f444a6147763c57db456959821
michalspano <michal.spano03@gmail.com>
2023-09-30 17:45:06 +0200

    Update `Array` declaration in `Main.java#main`

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   // The main entry point of the program. Test your code here!
-  public static void main(String args[]) {
+  public static void main(String[] args) {
     Repository codeCoffee = new Repository("SKIP - SkipFlow");
     Commit commit1 = new Commit("Add README.md", "Michal Spano", "26/09/2023");
     Commit commit2 = new Commit("Add Main.java", "Erik Harring", "27/09/2023", HashType.ADVANCED);
